56ae7cfc7237ec8a833b1426bba7da20cbf2903b
h70076111 <h70076222@2980.com>
2025-08-10 20:24:54 +0800

    Create board.h

---
+++ trunk/configs/boards/NX18NNW/board.h
@@ -0,0 +1,20 @@
+/* NX18NVNW */
+
+#define BOARD_PID		"NX18NVNW"
+#define BOARD_NAME		"NX18NVNW"
+#define BOARD_DESC		"NX18NVNW Router"
+#define BOARD_VENDOR_NAME	"H3c Nx18nvnw"
+#define BOARD_VENDOR_URL	"https://www.h3c.com/"
+#define BOARD_MODEL_URL		"https://www.h3c.com/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC		0
+#define BOARD_NUM_ANT_5G_TX		0
+#define BOARD_NUM_ANT_5G_RX		0
+#define BOARD_NUM_ANT_2G_TX		0
+#define BOARD_NUM_ANT_2G_RX		0
+#define BOARD_NUM_ETH_LEDS		0
+#define BOARD_NUM_ETH_EPHY		4
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
