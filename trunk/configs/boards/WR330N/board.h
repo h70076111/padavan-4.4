3985ac7fa42cc012a54977c8e58b1e463aa50b47
h70076111 <h70076222@2980.com>
2025-11-21 22:42:20 +0800

    Create board.h

---
+++ trunk/configs/boards/WR330N/board.h
@@ -0,0 +1,21 @@
+/* YOUHUA WR330N */
+
+#define BOARD_PID		"WR330N"
+#define BOARD_NAME		"WR330N"
+#define BOARD_DESC		"YOUHUA WR330N Wireless Router"
+#define BOARD_VENDOR_NAME	"Youhua Technology"
+#define BOARD_VENDOR_URL	"http://www.youhuatech.com/"
+#define BOARD_MODEL_URL		"http://www.youhuatech.com/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+
+#define BOARD_HAS_5G_11AC	0
+#define BOARD_NUM_ANT_5G_TX	0
+#define BOARD_NUM_ANT_5G_RX	0
+#define BOARD_NUM_ANT_2G_TX	0
+#define BOARD_NUM_ANT_2G_RX	0
+#define BOARD_NUM_ETH_LEDS	1
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
+#define BOARD_NUM_UPHY_USB3	0
+#define BOARD_USB_PORT_SWAP	0
