6e57079f1ccbe637123073b0b77a1ef54e1c13b3
h70076111 <h70076222@2980.com>
2025-11-28 20:50:34 +0800

    Update et.sh

--- trunk/user/etink/et.sh
+++ trunk/user/etink/et.sh
@@ -30,6 +30,8 @@ log() {
 }
 etink_keyg=$(nvram get etink_keyg)
 echo $etink_keyg
+etink_pass=$(nvram get etink_pass)
+echo $etink_pass
 etink_log=$(nvram get etink_log)
 echo $etink_log
 etink_log2=$(nvram get etink_log2)
