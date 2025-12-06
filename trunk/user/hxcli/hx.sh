c551c3c9ed9511c6d261872439bc19ba2472fefc
h70076111 <h70076222@2980.com>
2025-12-07 00:55:47 +0800

    Update hx.sh

--- trunk/user/hxcli/hx.sh
+++ trunk/user/hxcli/hx.sh
@@ -64,13 +64,22 @@ if [ "$1" = "x" ] ; then
 	fi
 	[ -f $relock ] && rm -f $relock
 fi
-start_hxcli
-}
+start_hxcli() {
 
 	[ "$hxcli_enable" = "0" ] && exit 1
 	logger -t "【HX客户端】" "正在启动hx-cli"
-
-hxclicmd="/usr/bin/hx-cli -k $hxcli_token $hxcli_serverw -d $hxcli_desname --nic hxsdwan -i $hxcli_localadd -o $lan_ipaddr/24 --ip $hxcli_ip >/tmp/hx-cli.log 2>&1"   
+CMD="/usr/bin/hx-cli -k $hxcli_token $hxcli_serverw -d $hxcli_desname --nic hxsdwan"
+	routenum=`nvram get hxcli_routenum_x`
+	for r in $(seq 1 $routenum)
+	do
+		i=`expr $r - 1`
+		hx_route=`nvram get hxcli_route_x$i`
+		hx_ip=`nvram get hxcli_ip_x$i`
+		hx_peer="${hx_route},${hx_ip}"
+		hx_peer="$(echo $hx_peer | tr -d ' ')"
+		CMD="${CMD} -i ${hx_peer}"
+	done	
+hxclicmd="${CMD} -o $lan_ipaddr/24 --ip $hxcli_ip >/tmp/hx-cli.log 2>&1"   
 
 echo "$hxclicmd" >/tmp/hx-cli.CMD 
 logger -t "【宏兴智能组网】" "运行${hxclicmd}"
@@ -84,6 +93,11 @@ iptables -I INPUT -i hxsdwan -j ACCEPT
 iptables -I FORWARD -i hxsdwan -o hxsdwan -j ACCEPT
 iptables -I FORWARD -i hxsdwan -j ACCEPT
 iptables -t nat -I POSTROUTING -o hxsdwan -j MASQUERADE
+	[ "$hxcli_proxy" = "1" ] && sysctl -w net.ipv4.ip_forward=1 >/dev/null 2>&1
+	if [ ! -z "$hx_tcp_port" ] ; then
+		 iptables -I INPUT -p tcp --dport $hx_tcp_port -j ACCEPT
+		 ip6tables -I INPUT -p tcp --dport $hx_tcp_port -j ACCEPT
+	fi
 #开启arp
 ifconfig hxcli arp
 else
@@ -107,6 +121,7 @@ cat >> "/tmp/script/_opt_script_check" <<-OSC
 OSC
 fi
 fi
+}
 
 stop_hx() {
 	logger -t "【HX客户端】" "正在关闭hx-cli..."
@@ -157,3 +172,20 @@ hx_status() {
         fi
 	exit 1
 }
+
+case $1 in
+start)
+	start_hxcli &
+	;;
+stop)
+	stop_hx
+	;;
+restart)
+	stop_hx
+	start_hxcli &
+	;;
+*)
+	echo "check"
+	#exit 0
+	;;
+esac
