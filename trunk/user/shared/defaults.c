7167649b74896158f2f8722a88a0225c724ef448
h70076111 <h70076222@2980.com>
2025-12-30 00:56:32 +0800

    Update defaults.c

--- trunk/user/shared/defaults.c
+++ trunk/user/shared/defaults.c
@@ -847,6 +847,7 @@ struct nvram_pair router_defaults[] = {
 	{ "et_system_enable", "0" },
 	{ "et_encryption_enable", "0" },
 	{ "et_thread_enable", "0" },
+	{ "et_dns_enable", "0" },
 	{ "etink_keyg", "abc123" },
 	{ "etink_pass", "abc123" },
 	{ "etink_xyip", "10.126.126.20" },
