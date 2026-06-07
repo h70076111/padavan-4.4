935ba3d33fb8927c9531dcbfefaed05bb2a46098
h70076111 <h70076222@2980.com>
2026-06-07 22:17:00 +0800

    Update etink.sh

--- trunk/user/etink/etink.sh
+++ trunk/user/etink/etink.sh
@@ -4,6 +4,8 @@ etink_keyg=$(nvram get etink_keyg)
 echo $etink_keyg
 etink_pass=$(nvram get etink_pass)
 echo $etink_pass
+etink_webul=$(nvram get etink_webul)
+echo $etink_webul
 etink_xyip=$(nvram get etink_xyip)
 echo $etink_xyip
 etink_log=$(nvram get etink_log)
@@ -209,7 +211,7 @@ if pidof easytier-core > /dev/null 2>&1; then
     exit 0
 fi
 
-CMD="$EASYTIER_BIN -w $etink_keyg --machine-id "$MACHINE_ID" &"
+CMD="$EASYTIER_BIN -w $etink_webul$etink_keyg --machine-id "$MACHINE_ID" &"
 
 echo $CMD
 log $CMD
